a44667b60c24e17758346faf238ae57a2a361675
Brian Cooke <brian.cooke@live.co.uk>
2018-06-27 13:32:01 +0100

    adds taking turn functionality and disabkes button after one click

--- src/containers/board.js
+++ src/containers/board.js
@@ -5,7 +5,8 @@ export default class Board extends Component {
   constructor(props){
     super(props);
     this.state = {
-      cells: [0,0,0,0,0,0,0,0,0]
+      cells: [0,0,0,0,0,0,0,0,0],
+      next: 1
     }
 
   this.handleButtonClick = this.handleButtonClick.bind(this);
@@ -13,11 +14,17 @@ export default class Board extends Component {
 
   handleButtonClick(event){
     const tempArray = this.state.cells
-    tempArray[event.target.value] = 2
-    console.log('handleButtonClick() event: ', event.target);
-    // console.log(tempArray);
-    // console.log("button clicked");
+    tempArray[event.target.value] = this.state.next
     this.setState({cells: tempArray})
+
+    if(this.state.next === 1 ){
+      this.setState({next: 2})
+    } else {
+      this.setState({next: 1})
+    }
+
+    event.target.disabled = true;
+
   }
 
   render() {
@@ -26,18 +33,18 @@ export default class Board extends Component {
       <table id = "board">
         <tr id="row-one">
           <td id = "cell-one"> <Button position='0' onClick={this.handleButtonClick} value={this.state.cells[0]}/> </td>
-          <td id = "cell-two"> <Button position='1'/> </td>
-          <td id = "cell-three"> <Button position='2'/> </td>
+          <td id = "cell-two"> <Button position='1' onClick={this.handleButtonClick} value={this.state.cells[1]}/> </td>
+          <td id = "cell-three"> <Button position='2' onClick={this.handleButtonClick} value={this.state.cells[2]}/> </td>
         </tr>
         <tr id="row-two">
-          <td id = "cell-four"> <Button position='3'/> </td>
-          <td id = "cell-five"> <Button/> </td>
-          <td id = "cell-six"> <Button/> </td>
+          <td id = "cell-four"> <Button position='3' onClick={this.handleButtonClick} value={this.state.cells[3]}/> </td>
+          <td id = "cell-five"> <Button position='4' onClick={this.handleButtonClick} value={this.state.cells[4]}/> </td>
+          <td id = "cell-six"> <Button position='5' onClick={this.handleButtonClick} value={this.state.cells[5]}/> </td>
         </tr>
         <tr id="row-three">
-          <td id = "cell-seven"> <Button/> </td>
-          <td id = "cell-eight"> <Button/> </td>
-          <td id = "cell-nine"> <Button/> </td>
+          <td id = "cell-seven"> <Button position='6' onClick={this.handleButtonClick} value={this.state.cells[6]}/> </td>
+          <td id = "cell-eight"> <Button position='7' onClick={this.handleButtonClick} value={this.state.cells[7]}/> </td>
+          <td id = "cell-nine"> <Button position='8' onClick={this.handleButtonClick} value={this.state.cells[8]}/> </td>
         </tr>
       </table>
       </div>
